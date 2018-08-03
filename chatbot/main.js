54f73b2ca454dc52eb5031b1820a1b577dbccdf6
jormora <39917627+jormora@users.noreply.github.com>
2018-08-02 21:26:11 -0500

    Add files via upload

---
+++ chatbot/main.js
@@ -0,0 +1,5 @@
+
+var header = document.getElementsByTagName("header")[0];
+var altura = header.clientHeight;
+var padding = altura.toString() + "px";
+document.getElementsByTagName("body")[0].style.paddingTop = padding;
